67d00db88b368703e642785a9a9588554d5214f7
Jytesh Punjwani <jyteshpunjwani@gmail.com>
2024-12-10 17:19:36 +0500

    update ExcelConstants testData path

--- src/framework/constants/ExcelConstants.ts
+++ src/framework/constants/ExcelConstants.ts
@@ -1,6 +1,6 @@
 export default class ExcelConstants {
     static readonly TEST_PATH = './src/resources/data/testData.xlsx';
-    static readonly SUITE_PATH = '../../resources/data/testData.xlsx';
+    static readonly SUITE_PATH = '../../10pplaywright-framework/src/resources/data/testData.xlsx';
     static readonly YES = "YES";
-    //C:\Users\jytesh.punjwani\Documents\playwright-framework\src\resources\data\testData.xlsx
+    //C:\Users\jytesh.punjwani\Documents\GitHub\10pplaywright-framework\src\resources\data\testData.xlsx
 }
